3a4f4524a7ea1b9b378159fb1edaf3af583a471e
crefter <crefter1@yandex.ru>
2022-01-15 22:05:30 +0700

    Deleted base

--- lib/data/api/currency_api.dart
+++ lib/data/api/currency_api.dart
@@ -2,7 +2,6 @@ import 'package:currency_app/data/dto/currency_rates_response.dart';
 import 'package:currency_app/data/dto/currency_response.dart';
 
 abstract class CurrencyApi {
-  static const String base = 'https://currencyapi.net';
 
   Future<CurrencyResponse> getCurrencies();
 
