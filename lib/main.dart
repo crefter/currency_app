3bad62e3bc9e86be940907b1b60d14b0e8e7fc43
crefter <crefter1@yandex.ru>
2022-01-18 21:22:12 +0700

    Renamed variable "getIt" to "get"

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:currency_app/data/api/currency_api.dart';
 import 'package:currency_app/data/api/currency_api_impl.dart';
 import 'package:currency_app/domain/repositories/currency_repository.dart';
 import 'package:currency_app/data/repositories/currency_repository_impl.dart';
+import 'package:currency_app/domain/usecases/find_rate_for_currency_use_case.dart';
+import 'package:currency_app/domain/usecases/load_currencies_use_case.dart';
+import 'package:currency_app/domain/usecases/load_rates_for_currency_use_case.dart';
 import 'package:currency_app/views/my_app.dart';
 import 'package:currency_app/views/navigation/main_navigation.dart';
 import 'package:currency_app/views/screens/screen_factory/screen_factory.dart';
@@ -10,21 +13,31 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 void setupDependencies() {
-  final getIt = GetIt.instance;
+  final get = GetIt.instance;
 
   //views
-  getIt.registerSingleton(ScreenFactory());
-  getIt.registerSingleton(MainNavigation());
+  get.registerSingleton(ScreenFactory());
+  get.registerSingleton(MainNavigation());
 
   //network
-  getIt.registerSingleton(Dio());
+  get.registerSingleton(Dio());
 
   //api
-  getIt.registerLazySingleton<CurrencyApi>(() => CurrencyApiImpl(getIt<Dio>()));
+  get.registerLazySingleton<CurrencyApi>(() => CurrencyApiImpl(get<Dio>()));
 
   //repositories
-  getIt.registerLazySingleton<CurrencyRepository>(
-      () => CurrencyRepositoryImpl(getIt<CurrencyApi>()));
+  get.registerLazySingleton<CurrencyRepository>(
+      () => CurrencyRepositoryImpl(get<CurrencyApi>()));
+
+  //use cases
+  get.registerLazySingleton(
+      () => LoadRatesForCurrencyUseCase(get<CurrencyRepository>()));
+  get.registerLazySingleton(
+      () => LoadCurrenciesUseCase(get<CurrencyRepository>()));
+  get.registerLazySingleton(
+      () => FindRateForCurrencyUseCase(get<LoadRatesForCurrencyUseCase>()));
+
+  //blocs
 }
 
 void main() {
