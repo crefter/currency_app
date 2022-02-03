07608b26c0afce037e4ed86537baf25901b53740
crefter <crefter1@yandex.ru>
2022-02-03 19:48:59 +0700

    Added consts and strings

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,4 +1,10 @@
 abstract class Consts {
-  static const appName = 'Currency';
-  static const mainPageName = 'Currency app';
+  static const double heightRatesList = 400;
+  static const int digitAfterDecimalPoint = 2;
+  static const double currencyPagePadding = 12.0;
+  static const double defaultWidthOfGap = 10.0;
+  static const double defaultHeightOfGap = 10.0;
+  static const double heightBetweenCurrencyAndRateWidgets = 50.0;
+  static const double defaultBorderRadius = 10.0;
+  static const double appBarElevation = 10.0;
 }
---
+++ lib/strings.dart
@@ -0,0 +1,8 @@
+abstract class Strings {
+  static const appName = 'Currency';
+  static const mainPageName = 'Currency app';
+  static const enterCurrencyHint = 'Введите валюту...';
+  static const rateHint = 'Рейты:';
+  static const changeLabel = 'Поменять';
+  static const currencyLabel = 'Курс';
+}
