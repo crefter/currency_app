988441407dd4a494339134815268bf162f1cea3b
crefter <crefter1@yandex.ru>
2022-04-01 21:43:42 +0700

    Refactored convert repository tests from api to data sources

--- test/data/repositories/convert_repository_test.dart
+++ test/data/repositories/convert_repository_test.dart
@@ -1,5 +1,7 @@
-import 'package:currency_app/data/api/convert_api.dart';
+import 'package:currency_app/data/datasource/local/convert_local_data_source.dart';
+import 'package:currency_app/data/datasource/remote/convert_remote_data_source.dart';
 import 'package:currency_app/data/dto/convert_response.dart';
+import 'package:currency_app/data/errors/convert_api_exception.dart';
 import 'package:currency_app/data/repositories/convert_repository_impl.dart';
 import 'package:currency_app/domain/entities/conversion.dart';
 import 'package:currency_app/domain/entities/currency.dart';
@@ -8,51 +10,57 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import '../api/convert_api_test.mocks.dart';
 import 'convert_repository_test.mocks.dart';
 
-@GenerateMocks([ConvertRepository])
+@GenerateMocks([
+  ConvertRepository,
+  ConvertLocalDataSource,
+  ConvertRemoteDataSource,
+])
 Future<void> main() async {
   group('Convert repository tests', () {
-    final ConvertRepository mockRepository = MockConvertRepository();
-    final ConvertApi mockConvertApi = MockConvertApi();
+    final mockRepository = MockConvertRepository();
+    final mockConvertLocalDataSource = MockConvertLocalDataSource();
+    final mockConvertRemoteDataSource = MockConvertRemoteDataSource();
     late ConvertRepository repository;
 
     test('should convert from currency to currency', () async {
       when(mockRepository.convert(
-              amount: 10,
-              from: const Currency('USD', 'country'),
-              to: const Currency('EUR', ''),))
-          .thenAnswer((_) async => Conversion(10, 'USD', 'EUR', 8));
+        amount: 10,
+        from: const Currency('USD', 'country'),
+        to: const Currency('EUR', ''),
+      )).thenAnswer((_) async => const Conversion(10, 'USD', 'EUR', 8));
       final actual = await mockRepository.convert(
-          amount: 10,
-          from: const Currency('USD', 'country'),
-          to: const Currency('EUR', ''),);
-      final expected = Conversion(10, 'USD', 'EUR', 8);
+        amount: 10,
+        from: const Currency('USD', 'country'),
+        to: const Currency('EUR', ''),
+      );
+      const expected = Conversion(10, 'USD', 'EUR', 8);
       expect(actual, expected);
     });
 
     test('should not convert from currency to currency', () async {
       when(mockRepository.convert(
-              amount: 10,
-              from: const Currency('USD', 'country'),
-              to: const Currency('EUR', ''),))
-          .thenAnswer((_) async => Conversion(10, 'USD', 'EUR', 8));
+        amount: 10,
+        from: const Currency('USD', 'country'),
+        to: const Currency('EUR', ''),
+      )).thenAnswer((_) async => const Conversion(10, 'USD', 'EUR', 8));
       final answer = await mockRepository.convert(
-          amount: 10,
-          from: const Currency('USD', 'country'),
-          to: const Currency('EUR', ''),);
-      final pattern = Conversion(10, 'USD', 'EUR', 6);
+        amount: 10,
+        from: const Currency('USD', 'country'),
+        to: const Currency('EUR', ''),
+      );
+      const pattern = Conversion(10, 'USD', 'EUR', 6);
 
       final actual = answer == pattern;
       expect(actual, false);
     });
 
-    test('should convert from usd to eur', () async {
-      when(mockConvertApi.convert(
-        amount: 10,
-        from: 'USD',
-        to: 'EUR',
+    test('should convert from usd to eur by remote', () async {
+      when(mockConvertRemoteDataSource.getResponse(
+        amount: anyNamed('amount'),
+        from: anyNamed('from'),
+        to: anyNamed('to'),
         output: anyNamed('output'),
       )).thenAnswer((_) async => ConvertResponse(
             conversion: ConversionResponse(
@@ -62,13 +70,52 @@ Future<void> main() async {
               result: 8,
             ),
           ));
-      repository = ConvertRepositoryImpl(mockConvertApi);
+      repository = ConvertRepositoryImpl(
+        mockConvertLocalDataSource,
+        mockConvertRemoteDataSource,
+      );
       final actual = await repository.convert(
         amount: 10,
         from: const Currency('USD', ''),
         to: const Currency('EUR', ''),
       );
-      final expected = Conversion(10, 'USD', 'EUR', 8);
+      const expected = Conversion(10, 'USD', 'EUR', 8);
+
+      expect(actual, expected);
+    });
+
+    test('should convert from usd to eur by local', () async {
+      when(mockConvertLocalDataSource.getResponse(
+        amount: anyNamed('amount'),
+        from: anyNamed('from'),
+        to: anyNamed('to'),
+        output: anyNamed('output'),
+      )).thenAnswer((realInvocation) async => ConvertResponse(
+            conversion: ConversionResponse(
+              amount: 10,
+              from: 'USD',
+              to: 'EUR',
+              result: 8,
+            ),
+          ));
+      when(mockConvertRemoteDataSource.getResponse(
+        amount: anyNamed('amount'),
+        from: anyNamed('from'),
+        to: anyNamed('to'),
+        output: anyNamed('output'),
+      )).thenThrow(ConvertApiException('message', 'description'));
+
+      repository = ConvertRepositoryImpl(
+        mockConvertLocalDataSource,
+        mockConvertRemoteDataSource,
+      );
+
+      final actual = await repository.convert(
+        amount: 10,
+        from: const Currency('USD', ''),
+        to: const Currency('EUR', ''),
+      );
+      const expected = Conversion(10, 'USD', 'EUR', 8);
 
       expect(actual, expected);
     });
